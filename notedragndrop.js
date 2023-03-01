0e42cce8730b6f3afb5ec863de13fdcfb88067f0
Driane <driane.dilao@symph.co>
2023-03-01 13:13:25 +0800

    First Cleanup Attempt

--- notedragndrop.js
+++ notedragndrop.js
@@ -1,59 +1,66 @@
-function dragndropNoteCard(){
+function dragndropNoteCard() {
+  const noteCardDraggables = document.querySelectorAll(".notecard-draggable");
+  const noteCardContainers = document.querySelectorAll(".notecard-container");
 
-    const noteCardDraggables = document.querySelectorAll(".notecard-draggable");
-    const noteCardContainers = document.querySelectorAll(".notecard-container");
-
-    // Event listeners for each noteCardDraggable
-    noteCardDraggables.forEach(noteCardDraggable => {
-        noteCardDraggable.addEventListener('dragstart', () => {
-            noteCardDraggable.classList.add("notecard-dragging");
-        });
-        noteCardDraggable.addEventListener('dragend', () => {
-            noteCardDraggable.classList.remove("notecard-dragging");
-        });
-        
+  // Event listeners for each noteCardDraggable
+  noteCardDraggables.forEach((noteCardDraggable) => {
+    noteCardDraggable.addEventListener("dragstart", () => {
+      noteCardDraggable.classList.add("notecard-dragging");
     });
-
-    // Event listeners for each noteCardContainer
-    noteCardContainers.forEach(noteCardContainer => {
-        noteCardContainer.addEventListener("dragover", event => {
-            event.preventDefault();
-
-            // afterNoteCardElement will have the element that is after the dragged element
-            const afterNoteCardElement = getDragAfterNoteCardElement(noteCardContainer, event.clientY);
-            const draggable = document.querySelector(".notecard-dragging");
-
-            // if there is no element after the dragged element, then append to end of list
-            // else, insert it before the element that is after the dragged element
-            if(afterNoteCardElement == null){
-                noteCardContainer.appendChild(draggable);
-            } else {
-                noteCardContainer.insertBefore(draggable, afterNoteCardElement);
-            }
-        });
+    noteCardDraggable.addEventListener("dragend", () => {
+      noteCardDraggable.classList.remove("notecard-dragging");
     });
+  });
 
-    //Helper function to return what element is after the dragged element
-    function getDragAfterNoteCardElement(container, y){
+  // Event listeners for each noteCardContainer
+  noteCardContainers.forEach((noteCardContainer) => {
+    noteCardContainer.addEventListener("dragover", (event) => {
+      event.preventDefault();
 
-        //List of elements that are currently undragged
-        const draggableElements = [...container.querySelectorAll(".notecard-draggable:not(.notecard-dragging)")]
+      // afterNoteCardElement will have the element that is after the dragged element
+      const afterNoteCardElement = getDragAfterNoteCardElement(
+        noteCardContainer,
+        event.clientY
+      );
+      const draggable = document.querySelector(".notecard-dragging");
 
-        return draggableElements.reduce((closest, child) => {
-            const box = child.getBoundingClientRect();
+      // if there is no element after the dragged element, then append to end of list
+      // else, insert it before the element that is after the dragged element
+      if (afterNoteCardElement == null) {
+        noteCardContainer.appendChild(draggable);
+      } else {
+        noteCardContainer.insertBefore(draggable, afterNoteCardElement);
+      }
+    });
+  });
 
-            /*Calculates the horizontal offset between the top of the element and the current mouse position 
+  //Helper function to return what element is after the dragged element
+  function getDragAfterNoteCardElement(container, yMouse) {
+    //List of elements that are currently undragged
+    const draggableElements = [
+      ...container.querySelectorAll(
+        ".notecard-draggable:not(.notecard-dragging)"
+      ),
+    ];
+
+    return draggableElements.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+
+        /*Calculates the horizontal offset between the top of the element and the current mouse position 
             of the user. If the offset is less than 0 (i.e. the mouse is at before/top of the element) and greater 
             than the offset of the previous closest element, it updates the closest variable to be the current 
             element using return { offset: offset, element: child }. Otherwise, it leaves closest unchanged. 
             Finally, it returns the element with the smallest offset using .element on the closest object.*/
-            const offset = y - box.top - box.height / 2;
+        const offset = yMouse - box.top - box.height / 2;
 
-            if(offset < 0 && offset > closest.offset) {
-                return { offset: offset, element: child }
-              } else {
-                return closest
-              }
-            }, { offset: Number.NEGATIVE_INFINITY }).element;
-    }
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      { offset: Number.NEGATIVE_INFINITY }
+    ).element;
+  }
 }
--- script.js
+++ script.js
@@ -1,150 +1,154 @@
 const newListButton = document.getElementById("add-list-button");
 const newListInput = document.getElementById("add-list-input");
-newListButton.addEventListener("click", function(){
-    addNewCardList(newListInput.value);
+newListButton.addEventListener("click", function () {
+  addNewCardList(newListInput.value);
 });
 
 function addNewCardList(title) {
-    
-    // Sets input value to "" whenever new card is generated
-    const addListInputValue = document.getElementById("add-list-input");
+  // Sets input value to "" whenever new card is generated
+  const addListInputValue = document.getElementById("add-list-input");
+  addListInputValue.value = "";
+
+  const deleteInputButton = document.getElementById("delete-button");
+  deleteInputButton.addEventListener("click", function () {
     addListInputValue.value = "";
+  });
 
-    const deleteInputButton = document.getElementById("delete-button");
-    deleteInputButton.addEventListener("click", function(){
-        addListInputValue.value = "";
-    });
+  //Get access to parent div named dragworkspace
+  const dragworkspace = document.getElementById("dragworkspace");
 
-    //Get access to parent div named dragworkspace
-    const dragworkspace = document.getElementById("dragworkspace");
+  // Create group of HTML represents that represents a card-list
+  const list = document.createElement("div");
+  list.classList.add("list");
 
-    // Create group of HTML represents that represents a card-list
-    const list = document.createElement("div");
-    list.classList.add("list");
-    
-    const cardList = document.createElement("div");
-    cardList.classList.add("card-list");
-  
-    const cardHeader = document.createElement("div");
-    cardHeader.classList.add("card-header");
-  
-    const cardHeaderTitle = document.createElement("input");
-    cardHeaderTitle.type = "text";
-    cardHeaderTitle.id = "my-textfield";
-    cardHeaderTitle.placeholder = "Heading";
-    cardHeaderTitle.value = title;
-  
-    const cardMenuButton = document.createElement("button");
-    cardMenuButton.classList.add("card-menu");
-  
-    const cardMenuIcon = document.createElement("i");
-    cardMenuIcon.classList.add("fas", "fa-trash-alt");
+  const cardList = document.createElement("div");
+  cardList.classList.add("card-list");
 
-    const cardContainer = document.createElement("div");
-    cardContainer.classList.add("notecard-container");//
-  
-    const cardFooter = document.createElement("div");
-    cardFooter.classList.add("card-footer");
-  
-    const cardButton = document.createElement("button");
-    cardButton.classList.add("card-button");
-  
-    const cardButtonIcon = document.createElement("i");
-    cardButtonIcon.classList.add("fa-duotone", "fa-plus");
-  
-    const cardButtonText = document.createElement("p");
-    cardButtonText.textContent = "Add a card";
+  const cardHeader = document.createElement("div");
+  cardHeader.classList.add("card-header");
 
-    cardHeader.appendChild(cardHeaderTitle);
-    cardMenuButton.appendChild(cardMenuIcon);
-    cardHeader.appendChild(cardMenuButton);
+  const cardHeaderTitle = document.createElement("input");
+  cardHeaderTitle.type = "text";
+  cardHeaderTitle.id = "my-textfield";
+  cardHeaderTitle.placeholder = "Heading";
+  cardHeaderTitle.value = title;
 
-    cardFooter.appendChild(cardButton);
-    cardButton.appendChild(cardButtonIcon);
-    cardButton.appendChild(cardButtonText);
-  
-    cardList.appendChild(cardHeader);
-    cardList.appendChild(cardContainer);
-    cardList.appendChild(cardFooter);
-    list.appendChild(cardList);
-    dragworkspace.appendChild(list);
+  const cardMenuButton = document.createElement("button");
+  cardMenuButton.classList.add("card-menu");
 
-    cardHeaderTitle.focus();
+  const cardMenuIcon = document.createElement("i");
+  cardMenuIcon.classList.add("fas", "fa-trash-alt");
 
-    // Add event listener to the "delete card list" button
-    cardMenuIcon.addEventListener("click", function() {
-      const confirmed = window.confirm("Do you wish to delete this card?");
-      if (confirmed) {
-        list.remove(); //Remove entire card list
-      } else {}
-    });
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("notecard-container");
 
-    // Add click event listener to the "Add a card note" button
-    cardButton.addEventListener("click", newCardBody);
+  const cardFooter = document.createElement("div");
+  cardFooter.classList.add("card-footer");
 
-    //A function that adds a body/note in between the cardHeader and cardFooter
-    //Declared this function inside because this will only be used inside this function
-    function newCardBody(){
-        // Creates a new group of HTML elements that represents the card body/notes
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-        cardBody.classList.add("notecard-draggable");//
-        cardBody.setAttribute("draggable", true);//
-        const noteInput = document.createElement("textarea");
-        noteInput.type = "text";
-        noteInput.id = "my-note-textfield";
-        noteInput.placeholder = "Note";
-        noteInput.disabled = false;
-        const editButton = document.createElement("button");
-        editButton.classList.add("card-button");
-        const editButtonIcon = document.createElement("i");
-        editButtonIcon.classList.add("fa-regular", "fa-pen-to-square", "hover-icon");
-        const deleteButton = document.createElement("button");
-        deleteButton.classList.add("card-button");
-        const deleteButtonIcon = document.createElement("i");
-        deleteButtonIcon.classList.add("fa-solid", "fa-x", "hover-icon");
+  const cardButton = document.createElement("button");
+  cardButton.classList.add("card-button");
 
-        editButton.appendChild(editButtonIcon);
-        deleteButton.appendChild(deleteButtonIcon);
-        cardBody.appendChild(noteInput);
-        cardBody.appendChild(editButton);
-        cardBody.appendChild(deleteButton);
-        
-        //Inserts the new note/body before the footer.
-        cardContainer.appendChild(cardBody);
-        noteInput.focus();
+  const cardButtonIcon = document.createElement("i");
+  cardButtonIcon.classList.add("fa-duotone", "fa-plus");
 
-        /* Whenever the user inputs text, we first set the height of the textarea to auto to
+  const cardButtonText = document.createElement("p");
+  cardButtonText.textContent = "Add a card";
+
+  cardHeader.appendChild(cardHeaderTitle);
+  cardMenuButton.appendChild(cardMenuIcon);
+  cardHeader.appendChild(cardMenuButton);
+
+  cardFooter.appendChild(cardButton);
+  cardButton.appendChild(cardButtonIcon);
+  cardButton.appendChild(cardButtonText);
+
+  cardList.appendChild(cardHeader);
+  cardList.appendChild(cardContainer);
+  cardList.appendChild(cardFooter);
+  list.appendChild(cardList);
+  dragworkspace.appendChild(list);
+
+  cardHeaderTitle.focus();
+
+  // Add event listener to the "delete card list" button
+  cardMenuIcon.addEventListener("click", function () {
+    const confirmed = window.confirm("Do you wish to delete this card?");
+    if (confirmed) {
+      list.remove(); //Removes entire card list
+    } else {
+    }
+  });
+
+  // Add click event listener to the "Add a card note" button
+  cardButton.addEventListener("click", newCardBody);
+
+  //A function that adds a body/note in between the cardHeader and cardFooter
+  //Declared this function inside because this will only be used inside this function
+  function newCardBody() {
+    // Creates a new group of HTML elements that represents the card body/notes
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    cardBody.classList.add("notecard-draggable");
+    cardBody.setAttribute("draggable", true);
+    const noteInput = document.createElement("textarea");
+    noteInput.type = "text";
+    noteInput.id = "my-note-textfield";
+    noteInput.placeholder = "Note";
+    noteInput.disabled = false;
+    const editButton = document.createElement("button");
+    editButton.classList.add("card-button");
+    const editButtonIcon = document.createElement("i");
+    editButtonIcon.classList.add(
+      "fa-regular",
+      "fa-pen-to-square",
+      "hover-icon"
+    );
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("card-button");
+    const deleteButtonIcon = document.createElement("i");
+    deleteButtonIcon.classList.add("fa-solid", "fa-x", "hover-icon");
+
+    editButton.appendChild(editButtonIcon);
+    deleteButton.appendChild(deleteButtonIcon);
+    cardBody.appendChild(noteInput);
+    cardBody.appendChild(editButton);
+    cardBody.appendChild(deleteButton);
+
+    //Inserts the new note/body before the footer.
+    cardContainer.appendChild(cardBody);
+    noteInput.focus();
+
+    /* Whenever the user inputs text, we first set the height of the textarea to auto to
          reset its height, and then set it to the scrollHeight of the textarea, which is 
          the height of the content including any overflow. */
-        noteInput.addEventListener('input', function(){
-            noteInput.style.height = 'auto';
-            noteInput.style.height = `${noteInput.scrollHeight}px`;
-        });
+    noteInput.addEventListener("input", function () {
+      noteInput.style.height = "auto";
+      noteInput.style.height = `${noteInput.scrollHeight}px`;
+    });
 
-        //Toggles the disabled property of the textarea element whenever we click the
-        //edit button
-        editButton.addEventListener("click", function() {
-            noteInput.disabled = false;
-            if (noteInput.disabled == false) {
-                noteInput.focus();
-            }
-        });
+    //Toggles the disabled property of the textarea element whenever we click the
+    //edit button
+    editButton.addEventListener("click", function () {
+      noteInput.disabled = false;
+      if (noteInput.disabled == false) {
+        noteInput.focus();
+      }
+    });
 
-        deleteButton.addEventListener("click", function() {
-            cardBody.remove();
-        });
+    deleteButton.addEventListener("click", function () {
+      cardBody.remove();
+    });
 
-        //Toggles the disabled property of the textarea element into true
-        //whenever the focus is not on the textarea element anymore
-        noteInput.addEventListener("blur", function(){
-            noteInput.disabled = true;
-        });
-
-        // To make sure that the elements are assigned to their classes before we run the notedragndrop file
-        dragndropNoteCard();
-    }
+    //Toggles the disabled property of the textarea element into true
+    //whenever the focus is not on the textarea element anymore
+    noteInput.addEventListener("blur", function () {
+      noteInput.disabled = true;
+    });
 
     // To make sure that the elements are assigned to their classes before we run the notedragndrop file
     dragndropNoteCard();
+  }
+
+  // To make sure that the elements are assigned to their classes before we run the notedragndrop file
+  dragndropNoteCard();
 }
